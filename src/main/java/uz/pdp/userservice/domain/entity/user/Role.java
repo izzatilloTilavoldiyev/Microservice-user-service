6add23bc86d94a338d96bcc1a747504330405310
izzatillotilavoldiyev5 <izzatillotilavoldiyev5@gmail.com>
2023-08-22 18:48:39 +0500

    entities updated

--- src/main/java/uz/pdp/userservice/domain/entity/user/Role.java
+++ src/main/java/uz/pdp/userservice/domain/entity/user/Role.java
@@ -1,20 +1,9 @@
 package uz.pdp.userservice.domain.entity.user;
 
-import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import uz.pdp.userservice.domain.entity.BaseEntity;
+public enum Role{
 
-
-@Entity(name = "roles")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Role extends BaseEntity {
-
-    private String name;
+    SUPER_ADMIN,
+    ADMIN,
+    USER
 
 }
--- src/main/java/uz/pdp/userservice/domain/entity/user/User.java
+++ src/main/java/uz/pdp/userservice/domain/entity/user/User.java
@@ -1,12 +1,12 @@
 package uz.pdp.userservice.domain.entity.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import uz.pdp.userservice.domain.entity.BaseEntity;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity(name = "users")
 @Getter
@@ -16,16 +16,18 @@ import java.util.List;
 @Builder
 public class User extends BaseEntity {
 
+    @Column(nullable = false)
     private String name;
 
     @Column(unique = true, nullable = false)
     private String email;
 
     @Column(nullable = false)
+    @JsonIgnore
     private String password;
 
-    @ManyToMany
-    private List<Role> roles;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Enumerated(EnumType.STRING)
     private UserState state;
@@ -34,4 +36,6 @@ public class User extends BaseEntity {
 
     @CreationTimestamp
     private LocalDateTime verificationDate;
+
+    private boolean deleted;
 }
